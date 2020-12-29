dd6e0894ffffc5da32eb3e0f3aea86b246537dfa
Theseus <heavenlywork@outlook.com>
2020-12-29 22:54:18 +0300

    61, auth, v0.26

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import h from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={h.header}>
-            <img src='https://image.shutterstock.com/image-vector/feminine-logo-set-black-pink-260nw-1150394132.jpg'></img>
+            <img
+                src='https://image.shutterstock.com/image-vector/feminine-logo-set-black-pink-260nw-1150394132.jpg'></img>
+            <div className={h.login}>
+                {props.isAuth ? props.login : <NavLink to={`/login`}>Login</ NavLink>}
+            </div>
         </header>
     )
 }
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -2,11 +2,13 @@ import {combineReducers, createStore} from "redux";
 import dialogsReducer from "./dialogsReducer";
 import profilesReducer from "./profilesReducer";
 import usersReducer from "./usersReducer";
+import authReducer from "./authReducer";
 
 let reducers = combineReducers({
     dialogsPage: dialogsReducer,
     profilePage: profilesReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 })
 
 let store = createStore(reducers);
