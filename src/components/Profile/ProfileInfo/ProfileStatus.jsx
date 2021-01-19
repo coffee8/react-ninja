346ddafd18a32fe4082232d35f03ca1170bb0fc4
Theseus <heavenlywork@outlook.com>
2021-01-19 21:20:40 +0300

    minor changes

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -7,12 +7,13 @@ class ProfileStatus extends React.Component {
         editMode: false,
     };
 
-    activateEditMode() {
+    activateEditMode = () => {
+        console.log("this: ", this);
         this.setState({
             editMode: true
         });
     }
-    deactivateEditMode() {
+    deactivateEditMode = () => {
         this.setState({
             editMode: false
         });
@@ -24,13 +25,13 @@ class ProfileStatus extends React.Component {
                 {
                     !this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                     </div>
                 }
                 {
                     this.state.editMode &&
                     <div>
-                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}/>
                     </div>
                 }
             </div>
